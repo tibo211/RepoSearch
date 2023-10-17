5530de063d48e9a5a9ad25d76b11f51ef500be0c
Felföldy Tibor <tibor.felfoldy@bcsconsult.hu>
2023-10-17 17:55:57 +0200

    Initial Commit

---
+++ RepoSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RepoSearch
+//
+//  Created by Tibor Felföldy on 2023-10-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RepoSearch/RepoSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoSearchApp.swift
+//  RepoSearch
+//
+//  Created by Tibor Felföldy on 2023-10-17.
+//
+
+import SwiftUI
+
+@main
+struct RepoSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
